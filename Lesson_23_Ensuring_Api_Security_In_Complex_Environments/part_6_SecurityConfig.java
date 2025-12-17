f709fc8581ae34ea09cb87130a67b5da8ad0435c
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-12-17 02:17:02 -0500

    Add Lesson_23_Ensuring_Api_Security_In_Complex_Environments/part_6_SecurityConfig.java

---
+++ Lesson_23_Ensuring_Api_Security_In_Complex_Environments/part_6_SecurityConfig.java
@@ -0,0 +1,14 @@
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/api/public/**").permitAll()
+            .antMatchers("/api/private/**").authenticated()
+            .and()
+            .httpBasic();
+    }
+}

2d516698cb3f008759c5edba8766c7aa6655cf40
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-12-17 02:18:30 -0500

    Add Lesson_3_Managing_Errors_In_A_Restful_Api/part_2_GetBookById.java

---
+++ Lesson_3_Managing_Errors_In_A_Restful_Api/part_2_GetBookById.java
@@ -0,0 +1,7 @@
+@GetMapping("/{id}")
+public ResponseEntity<BookDTO> getBookById(@PathVariable Long id) {
+    Book book = bookRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("Book not found with id: " + id));
+    BookDTO bookDTO = new BookDTO(book.getId(), book.getTitle(), book.getAuthor());
+    return ResponseEntity.ok(bookDTO);
+}

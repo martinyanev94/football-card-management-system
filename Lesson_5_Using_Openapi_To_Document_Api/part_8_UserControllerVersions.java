9096769b2d26e236ef49e84cc910275dc4e1337f
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-12-17 02:18:56 -0500

    Add Lesson_5_Using_Openapi_To_Document_Api/part_8_UserControllerVersions.java

---
+++ Lesson_5_Using_Openapi_To_Document_Api/part_8_UserControllerVersions.java
@@ -0,0 +1,11 @@
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserV1Controller {
+    // Endpoints for version 1
+}
+
+@RestController
+@RequestMapping("/api/v2/users")
+public class UserV2Controller {
+    // Endpoints for version 2 with new features
+}

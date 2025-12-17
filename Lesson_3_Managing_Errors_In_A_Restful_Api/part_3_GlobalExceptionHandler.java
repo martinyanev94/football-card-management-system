3e8b8020262a7d2a3a0218a3dd7b40429131ae78
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-12-17 02:18:31 -0500

    Add Lesson_3_Managing_Errors_In_A_Restful_Api/part_3_GlobalExceptionHandler.java

---
+++ Lesson_3_Managing_Errors_In_A_Restful_Api/part_3_GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", ex.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleGenericException(Exception ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", "An unexpected error occurred");
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}

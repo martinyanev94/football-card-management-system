e51993eea01212a14fd42f4df2e46b3be53bfb83
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-12-17 02:18:28 -0500

    Add Lesson_3_Managing_Errors_In_A_Restful_Api/part_1_ResourceNotFoundException.java

---
+++ Lesson_3_Managing_Errors_In_A_Restful_Api/part_1_ResourceNotFoundException.java
@@ -0,0 +1,6 @@
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}

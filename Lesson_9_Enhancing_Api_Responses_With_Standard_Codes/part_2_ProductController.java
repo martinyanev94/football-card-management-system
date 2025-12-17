d8e7fa2531c7eb8e02b7d769ce3f2efbaed838cc
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-12-17 02:19:18 -0500

    Add Lesson_9_Enhancing_Api_Responses_With_Standard_Codes/part_2_ProductController.java

---
+++ Lesson_9_Enhancing_Api_Responses_With_Standard_Codes/part_2_ProductController.java
@@ -0,0 +1,13 @@
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        Product createdProduct = productService.saveProduct(product);
+        return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
+    }
+}

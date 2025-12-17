b3daa87a04f6ed45bf46794df1c46d6d39ee0da0
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-12-17 02:17:37 -0500

    Add Lesson_26_Mocking_Api_Calls_For_Testing_Purposes/part_6_FootballServiceTest.java

---
+++ Lesson_26_Mocking_Api_Calls_For_Testing_Purposes/part_6_FootballServiceTest.java
@@ -0,0 +1,15 @@
+import org.junit.After;
+import org.junit.Before;
+
+public class FootballServiceTest extends BaseWireMockTest {
+
+    @Before
+    public void setup() {
+        resetAll();
+    }
+
+    @After
+    public void teardown() {
+        resetAll();
+    }
+}
